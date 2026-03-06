16b3cd4f8faf302cc5e7ec27ebc4142145bfe298
Ginej Neupane <grim98413@gmail.com>
2026-03-06 14:32:33 +0545

    feat(videolizer): add package init

---
+++ videolizer/__init__.py
@@ -0,0 +1,8 @@
+"""
+Videolizer: Media Remix Engine for MofaClaw.
+
+Renders premium video clips from a VideoPlan. Each module (voiceover, subtitles,
+images, video, music) can be called individually or composed into a full pipeline.
+"""
+
+__version__ = "0.1.0"
